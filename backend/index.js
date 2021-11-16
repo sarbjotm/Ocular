9167e91595ccf835518878033cbea2c0e461e74c
Oliver Xie <oliverxie19@gmail.com>
2021-11-16 00:13:53 -0800

    add connection pool as export (for session setup)

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,10 @@ let app = express();
 
 const pp_config = require('./passport');
 const db = require('./db');
+const useSessions = require('./users/sessions').useSessions;
 
 pp_config(passport, db);
+useSessions(app, passport, db);
 
 app.get('/', (req, res) => {
     res.send("Hi");
