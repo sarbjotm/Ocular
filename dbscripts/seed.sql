842bd846fdd81dcb8a71d9dd458040d3ca2d30de
Oliver Xie <oliverxie19@gmail.com>
2021-11-16 00:11:56 -0800

    update DB seeding to create sessions table

--- dbscripts/seed.sql
+++ dbscripts/seed.sql
@@ -1,5 +1,17 @@
 BEGIN;
 
+--From https://github.com/voxpelli/node-connect-pg-simple/blob/HEAD/table.sql (sessions)
+CREATE TABLE "session" (
+    "sid" varchar NOT NULL COLLATE "default",
+	"sess" json NOT NULL,
+	"expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+
 CREATE TABLE roles (
     id BIGSERIAL PRIMARY KEY,
     description VARCHAR(255) NOT NULL,
