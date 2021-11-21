69f5f6a932f1f8c70aa7f307165196e59dee9f4f
Oliver Xie <oliverxie19@gmail.com>
2021-11-20 16:31:02 -0800

    email checking using overly-generic regex

--- backend/users/index.js
+++ backend/users/index.js
@@ -8,6 +8,11 @@ const existingAccountQuery = 'SELECT username FROM users WHERE username=$1;';
 
 async function createAccount(req, res) {
     // Validate email
+    // This is general, can replace with a specific-domain email or other policy
+    const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    if (!emailRegex.test(req.body.email)) {
+        return res.send("Email is not valid.");
+    }
     // Validate username
     // Validate password (special characters/length requirements)
 
