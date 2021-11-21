09bd00baf26581ad8a830807560c263dfcff3820
hyl30 <hyl30@sfu.ca>
2021-11-21 14:39:40 -0800

    initial commit for pre-req tree component

---
+++ frontend/src/js/prereq-tree/Tree.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Tree = () => {
+    // https://www.sfu.ca/students/calendar/2022/spring/programs/computing-science/major/bachelor-of-science-or-bachelor-of-arts.html
+};
