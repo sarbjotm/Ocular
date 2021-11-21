9695008e38d95a27281061f96ff28cfbde4053bf
Oliver Xie <oliverxie19@gmail.com>
2021-11-20 16:02:59 -0800

    add view engine + move user tasks to separate router

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const PORT = process.env.PORT || 8080;
 
@@ -9,12 +10,16 @@ let app = express();
 const pp_config = require('./passport');
 const db = require('./db');
 const useSessions = require('./users/sessions').useSessions;
-const accessControl = require('./users/accessControl');
-const users = require('./users');
+const userRouter = require('./users/routes');
 
 app.set('trust proxy', 1);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
+// for development only (view prototyping)
+app.set('views', path.join(process.cwd(), 'views'));
+app.set('view engine', 'ejs');
+
 pp_config(passport, db);
 useSessions(app, passport, db);
 
@@ -22,16 +27,6 @@ app.get('/', (req, res) => {
     res.send("Hi");
 });
 
-app.post('/new', users.createAccount);
-
-app.post('/login', passport.authenticate('local'), (req, res) => {
-    res.send("Logged in");
-});
-
-app.get('/restricted', accessControl.requiresLogin, (req, res) => {
-    res.send("This is a protected resource!");
-});
-
-app.get('/logout', accessControl.requiresLogin, users.logout);
+app.use('/users', userRouter);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
---
+++ backend/users/routes.js
@@ -0,0 +1,27 @@
+let router = require('express').Router();
+const accessControl = require('./accessControl');
+const users = require('./index');
+const passport = require('passport');
+
+router.get('/new', (req, res) => {
+    res.render('newuser');
+});
+
+router.post('/new', users.createAccount);
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+    res.send("Logged in");
+});
+
+router.get('/restricted', accessControl.requiresLogin, (req, res) => {
+    res.send("This is a protected resource!");
+});
+
+router.get('/logout', accessControl.requiresLogin, users.logout);
+
+
+module.exports = router;
