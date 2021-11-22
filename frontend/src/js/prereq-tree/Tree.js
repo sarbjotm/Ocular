202da628f52e027ba5bdc0f3330cf46e7765169a
hyl30 <hyl30@sfu.ca>
2021-11-21 17:22:57 -0800

    added fetching methods

--- frontend/src/js/prereq-tree/Tree.js
+++ frontend/src/js/prereq-tree/Tree.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const Tree = () => {
-    // https://www.sfu.ca/students/calendar/2022/spring/programs/computing-science/major/bachelor-of-science-or-bachelor-of-arts.html
+    const prereqRegex = /<li class="prereq"> <h4>Prerequisite.+?<\/li>/;
+    const term = "spring";
+    const year = "2022";
+    const subject = "cmpt";
+    const code = "120";
+    
+    //const [outline, setOutline] = useState("");
+
+    useEffect(async () => {
+        try {
+            const sectionResponse = await fetch(`https://www.sfu.ca/bin/wcm/course-outlines?${year}/${term}/${subject}/${code}`);
+            const sectionData = await sectionResponse.json();
+            const section = sectionData[0]["value"]; // get the first available section
+            const outlineResponse = await fetch(`https://www.sfu.ca/outlines.html?${year}/${term}/${subject}/${code}/${section}`);
+            const outline = await outlineResponse.text();
+            const prereqLine = await outline.match(prereqRegex);
+            console.log(prereqLine);
+        } catch (error) {
+            console.error(error);
+        }
+
+        // get pre-req
+    }, []);
+
+    return(
+        <></>
+    );
 };
