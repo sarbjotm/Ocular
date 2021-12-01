bc063140eb0a321d427b88d4d7d33d2e941aacb7
hyl30 <hyl30@sfu.ca>
2021-12-01 13:42:48 -0800

    added prerequisite query route

--- backend/courses/index.js
+++ backend/courses/index.js
@@ -1,6 +1,7 @@
 const db = require('../db');
 
 const coursesQuery = 'SELECT * FROM courses;';
+const prerequisiteQuery = 'SELECT prerequisite FROM courses WHERE area=$1 AND code=$2;';
 
 async function courseList(req, res) {
     let coursesQueryResult = await db.query(coursesQuery);
@@ -8,6 +9,21 @@ async function courseList(req, res) {
     return;
 }
 
+async function getCoursePrerequisite(req, res) {
+    let area = req.body.area;
+    let code = req.body.code;
+
+    let prerequisiteQueryResult = await db.query(prerequisiteQuery, [area, code]);
+    if (!prerequisiteQueryResult.rows.length) {
+        res.status(404).send("Course cannot be found in system.");
+        return;
+    }
+
+    res.json(prerequisiteQueryResult.rows[0]);
+    return
+}
+
 module.exports = {
     courseList,
+    getCoursePrerequisite
 }
--- backend/courses/routes.js
+++ backend/courses/routes.js
@@ -3,4 +3,6 @@ const courses = require('./index');
 
 router.get('/', courses.courseList); // testing route for checking json is successfully loaded to database table
 
+router.get('/info', courses.getCoursePrerequisite);
+
 module.exports = router;
