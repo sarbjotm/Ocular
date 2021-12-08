2de059d052e1c458bbc52c0e99d987e0bab08e17
Sarbjot Mann <sarbjotm@sfu.ca>
2021-12-08 13:40:58 -0800

    Filtering courses for calendar view

--- backend/calendar/index.js
+++ backend/calendar/index.js
@@ -1,5 +1,5 @@
 const db = require('../db');
-const coursesQuery = 'SELECT * FROM courses;';
+const coursesQuery = 'SELECT * FROM courses WHERE code::INTEGER BETWEEN 100 AND 121';
 
 async function calendarList(req, res) {
     let coursesQueryResult = await db.query(coursesQuery);
