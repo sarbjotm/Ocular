41c053315683fd59deedf2730a3b32542c5445a4
Sarbjot Mann <sarbjotm@sfu.ca>
2021-12-07 22:36:59 -0800

    Removing Code not needed by calendar

--- backend/calendar/routes.js
+++ backend/calendar/routes.js
@@ -3,6 +3,5 @@ const calendar = require('./index');
 
 router.get('/', calendar.courseList); // testing route for checking json is successfully loaded to database table
 
-router.get('/info/:area/:code', calendar.getCoursePrerequisite);
 
 module.exports = router;
