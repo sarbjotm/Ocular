0a001cc3c50fab81b7d700f0ebcd9d092e095cf4
Sarbjot Mann <sarbjotm@sfu.ca>
2021-12-08 19:15:02 -0800

    Added Temp Time Check

--- backend/calendar/index.js
+++ backend/calendar/index.js
@@ -1,11 +1,18 @@
 const db = require('../db');
 const coursesQuery = 'SELECT * FROM courses WHERE code::INTEGER BETWEEN 100 AND 121';
+const courseTimes = []
 
 async function calendarList(req, res) {
     let coursesQueryResult = await db.query(coursesQuery);
-    res.render("calendarTime.ejs", { calendarList: coursesQueryResult.rows });
+    for (let i = 0; i < calendarList.length; i++){
+      courseTimes.push("11:00 - 12:00")
+    }
+
+    res.render("calendarTime.ejs", { calendarList: coursesQueryResult.rows, courseTimes: courseTimes });
     return;
 }
+
+
 
 module.exports = {
     calendarList
