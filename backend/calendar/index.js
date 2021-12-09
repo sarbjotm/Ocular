c2404daabb0af29e16f51a1f4a73807d62eb958c
Sarbjot Mann <sarbjotm@sfu.ca>
2021-12-09 00:20:44 -0800

    Calendar now shows time. Time added to course table

--- backend/calendar/index.js
+++ backend/calendar/index.js
@@ -1,14 +1,10 @@
 const db = require('../db');
 const coursesQuery = 'SELECT * FROM courses WHERE code::INTEGER BETWEEN 100 AND 121';
-const courseTimes = []
+
 
 async function calendarList(req, res) {
     let coursesQueryResult = await db.query(coursesQuery);
-    for (let i = 0; i < calendarList.length; i++){
-      courseTimes.push("11:00 - 12:00")
-    }
-
-    res.render("calendarTime.ejs", { calendarList: coursesQueryResult.rows, courseTimes: courseTimes });
+    res.render("calendarTime.ejs", { calendarList: coursesQueryResult.rows});
     return;
 }
 
