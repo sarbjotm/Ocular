1ae49fa6dfde7df6d303385f616b76a05ab97e82
Oliver Xie <oliverxie19@gmail.com>
2021-12-13 22:39:14 -0800

    require login for calendar/enrollment routes

--- backend/calendar/routes.js
+++ backend/calendar/routes.js
@@ -1,5 +1,8 @@
 let router = require('express').Router();
 const calendar = require('./index');
+const accessControl = require('../users/accessControl');
+
+router.use(accessControl.requiresLogin);
 
 router.get('/', calendar.calendarList); // testing route for checking json is successfully loaded to database tab
 
